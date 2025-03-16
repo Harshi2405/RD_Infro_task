ff23f3e72db1bee67f6400a509b6534002fde763
Harshitha Samatham <121963161+Harshi2405@users.noreply.github.com>
2025-03-16 18:19:10 +0530

    Add files via upload

---
+++ RD/script.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        if (targetElement) {
+            const navbarHeight = document.querySelector('header').offsetHeight;
+            const targetPosition = targetElement.offsetTop - navbarHeight;
+
+            window.scrollTo({
+                top: targetPosition,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
